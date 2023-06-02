7441f903c6b49f2b42cbe8b1d7ae5d1f9484bab6
LundPiyush <piyushlund349@gmail.com>
2023-06-03 05:13:10 +0530

    fix(): added toastify messages

--- src/components/CartProductCard/CartProductCard.jsx
+++ src/components/CartProductCard/CartProductCard.jsx
@@ -5,6 +5,7 @@ import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlin
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { useWishlist } from "../../contexts/wishlistContext";
 import { isItemInWishlist } from "../../utils/isItemInWishlist";
+import { toast } from "react-toastify";
 
 const CartProductCard = (product) => {
   const { _id, name, category, discount, price, qty, src } = product;
@@ -19,12 +20,16 @@ const CartProductCard = (product) => {
             className="wishlist-icon-card"
             onClick={() => {
               removeWishlistData(_id);
+              toast.success(`${product.name} removed from wishlist`);
             }}
           />
         ) : (
           <FavoriteBorderOutlinedIcon
             className="wishlist-icon-card"
-            onClick={() => addWishlistData(product)}
+            onClick={() => {
+              addWishlistData(product);
+              toast.success(`${product.name} added to wishlist`);
+            }}
           />
         )}
       </div>
@@ -41,19 +46,30 @@ const CartProductCard = (product) => {
         <div className="card-quanity">
           <button
             disabled={qty <= 1}
-            onClick={() => changeCartQuantity(_id, "decrement")}
+            onClick={() => {
+              changeCartQuantity(_id, "decrement");
+              toast.success(`Quantity updated successfully to ${qty - 1}`);
+            }}
             className="cart-btn-qty">
             -
           </button>
           <p>{qty}</p>
           <button
-            onClick={() => changeCartQuantity(_id, "increment")}
+            onClick={() => {
+              changeCartQuantity(_id, "increment");
+              toast.success(`Quantity updated successfully to ${qty + 1}`);
+            }}
             className="cart-btn-qty">
             +
           </button>
         </div>
 
-        <button className="cart-remove-btn" onClick={() => removeCartData(_id)}>
+        <button
+          className="cart-remove-btn"
+          onClick={() => {
+            removeCartData(_id);
+            toast.success(`${name} removed from cart successfully`);
+          }}>
           Remove from Cart
         </button>
       </div>
